fc5f3fc89ce3a30f328650f4f10f5783c49f0167
jtstockard <jtstockard92@gmail.com>
2021-06-24 16:02:08 -0500

    fixed formatting

--- config/connection.js
+++ config/connection.js
@@ -6,6 +6,7 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: "localhost",
+      port: 3001,
       password: "password",
       dialect: "mysql",
       dialectOptions: {
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -8,7 +8,7 @@ const postData = [
     user_id: 3,
   },
   {
-    title: "Zoo Keepr reaches 1 million subscribers!",
+    title: "Zoo Keeper reaches 1 million subscribers!",
     post_content:
       "The local zoo has received funding to build a new online catalog, and they've asked to create a web server for a front-end application they’re developing, called Zoo Keepr. This site’s data will be stored on the server. This will allow animal enthusiasts to access the data from different locations and browsers without needing to download it to their device",
     user_id: 1,
